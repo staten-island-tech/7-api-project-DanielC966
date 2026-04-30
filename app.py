6df948687ed82d85c3394860b1062b66b8ae3735
DanielC966 <danielc966@nycstudents.net>
2026-04-30 09:24:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import requests
 
 def getAbilities(agent):
-    response = requests.get(f"https://valorant-api.com/v1/agents/")
-    data = response.json()
+    agentResponse = requests.get(f"https://valorant-api.com/v1/agents/")
+    data = agentResponse.json()
     for i in data["data"]:
         if agent == i["displayName"]:
             for ability in i["abilities"]:
                 print(ability["displayName"])
+def getAgentList():
+    agentResponse = requests.get(f"https://valorant-api.com/v1/agents/")
+    data = agentResponse.json()
+    for i in 
     
     
 
